aecddbf775dc6cc37b8db189ef21859b35dd34b6
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:57:07 -0700

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,19 @@
+'''
+Write a program to find the sum of the elements in the array. 
+Input Format: 
+Input consists of n+1 integers where n corresponds to the number of elements in the array.
+The first integer corresponds to n and the next n integers correspond to the elements in the array.
+Assume that the maximum number of elements in the array is 20. 
+Output Format: 
+Output consists of a double value which corresponds to the mean of the array.
+Refer sample input and output for formatting specifications.
+Sample Input: 
+5
+2
+4
+1
+3
+1
+Sample Output:
+11
+'''
