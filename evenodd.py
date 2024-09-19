23b147798ac021cc6fa0973b2f0d78f35ed5d3d3
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:56:38 -0700

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,21 @@
+'''
+Write a Python Program to put the even and odd elements in a list into two different lists.
+Input format:
+Input consists of one integer and one list.
+First input consists of the size of the list.
+Second input consists of the elements based on the size.
+Output format:
+Output consists of two lists.
+First list consists of all the even numbers in the list.
+Second list consists of all the odd numbers in the list.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+The even list [2, 6]
+The odd list [1, 3, 5]
+'''
