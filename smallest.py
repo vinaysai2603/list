d7d3469251ffeabbcd7397126dfd67edccf72804
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:57:40 -0700

    Create smallest.py

---
+++ smallest.py
@@ -0,0 +1,17 @@
+'''
+Write a Python Program to find the smallest number in a list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the size of the list.
+Output consists of the largest element.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+1
+'''
