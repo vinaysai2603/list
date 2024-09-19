eb3e0242d2e91957f81e275f2d0dd6cb702ba60e
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:58:17 -0700

    Create search.py

---
+++ search.py
@@ -0,0 +1,20 @@
+'''
+Write a Program to find the search element in the given list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the elements of list in single line separated by space.
+Third input consists of the element which we need to search
+Sample Input 1:
+5
+1 2 3 6 5
+3
+Sample Output 1:
+3 is present in the given list
+Sample Input 2:
+5
+1 2 3 6 5
+4
+Sample Output 2:
+4 is not present in the given list
+'''
