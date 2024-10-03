d3d0cd3aa9fc8d907ea7c4274940339a84fc6217
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:37:07 -0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -5,3 +5,14 @@ Sample Input:
 Sample Output:
 50 40 30 20 10 
 '''
+def print_reverse(numbers):
+    # Print the numbers in reverse order
+    print(" ".join(map(str, reversed(numbers))))
+
+# Input
+input_data = input("Enter the numbers: ")  # Take input as a single line
+numbers = list(map(int, input_data.split()))  # Convert the input into a list of integers
+
+# Call the function to print the numbers in reverse order
+print_reverse(numbers)
+
