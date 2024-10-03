2a2fff6de0e18d9b474a1c2df6ebab1d0b202305
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:39:22 -0700

    Update largest.py

--- largest.py
+++ largest.py
@@ -15,3 +15,21 @@ Sample Input:
 Sample Output:
 6
 '''
+def find_largest_number(numbers):
+    return max(numbers)
+
+# Input
+size = int(input("Enter the size of the list: "))  # First input for the size of the list
+numbers = []
+
+# Collecting the numbers
+for _ in range(size):
+    num = int(input())
+    numbers.append(num)
+
+# Find the largest number
+largest_number = find_largest_number(numbers)
+
+# Output the largest number
+print(largest_number)
+
