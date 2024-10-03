46795a4833406c5807b6a6149823e5ce938405bc
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:33:41 -0700

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -15,3 +15,21 @@ Sample Input:
 Sample Output:
 1
 '''
+def find_smallest_number(numbers):
+    return min(numbers)
+
+# Input
+size = int(input("Enter the size of the list: "))  # First input for the size of the list
+numbers = []
+
+# Collecting the numbers
+for _ in range(size):
+    num = int(input())
+    numbers.append(num)
+
+# Find the smallest number
+smallest_number = find_smallest_number(numbers)
+
+# Output the smallest number
+print(smallest_number)
+
