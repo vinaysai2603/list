92206ad7512012826a6a5f84a5ec7175cbc2d173
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:32:54 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,3 +5,15 @@ Sample Input:
 Sample Output:
 10 20 30 40 50 
 '''
+def sort_and_print(numbers):
+    # Sort the list
+    sorted_numbers = sorted(numbers)
+    # Print the sorted list
+    print(" ".join(map(str, sorted_numbers)))
+
+# Input
+input_data = list(map(int, input("Enter the numbers: ").split()))
+
+# Call the function to sort and print the numbers
+sort_and_print(input_data)
+
